d6958b92b56998913c330bfe0282c0a83db8bf13
Ilya_Builuk_cp <SND\Ilya_Builuk_cp>
2013-11-21 11:42:03 -0800

    fixed build issue

---
+++ trunk/PhoneGuitarTab.UnitTests/Cloud/SkyDriveServiceTests.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Moq;
+using PhoneGuitarTab.Core.Services;
+using PhoneGuitarTab.Data;
+using PhoneGuitarTab.UI.Infrastructure;
+
+namespace PhoneGuitarTab.UnitTests.Cloud
+{
+    [TestClass]
+    public class SkyDriveServiceTests
+    {
+        private ICloudService _cloudService;
+        private IDataContextService _dataService;
+        private IsolatedStorageFileService _isoService;
+        private TabFileStorage _tabFileStorage;
+        private TestContext _testContext;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _dataService = CreateDataContext();
+            _testContext = new TestContext();
+        }
+
+       // [TestMethod]
+        public void CanSynchronize()
+        {
+            var group = _dataService.GetOrCreateGroupByName("Opeth");
+
+            var tabs = group.Tabs.ToList();
+        }
+
+
+        private IDataContextService CreateDataContext()
+        {
+            var dataMock = new Mock<IDataContextService>();
+
+            dataMock.Setup(ds => ds.GetOrCreateGroupByName(It.IsAny<string>())).Returns((string s) =>
+            {
+                var groupMock = new Mock<Group>();
+                groupMock.Setup(g => g.Tabs).Returns(() =>
+                {
+                    var tabs = new EntitySet<Tab>();
+                    tabs.AddRange(Enumerable.Range(1, 3).Select(i =>
+                    {
+                        var tabMock = new Mock<Tab>();
+                        tabMock.Setup(t => t.Id).Returns(i);
+                        tabMock.Setup(t => t.Name).Returns(string.Format("{0}.txt", s, i));
+                        return tabMock.Object;
+                    }));
+                    return tabs;
+                });
+
+                return groupMock.Object;
+            });
+
+            dataMock.Setup(ds => ds.InsertTab(It.IsAny<Tab>())).Callback((Tab tab) => _testContext.IsoInsertedTabs.Add(tab));
+
+            return dataMock.Object;
+        }
+
+
+        private class TestContext
+        {
+            public List<Tab> IsoInsertedTabs { get; set; }
+
+            public TestContext()
+            {
+                IsoInsertedTabs = new List<Tab>();
+                
+            }
+
+        }
+    }
+}
--- trunk/PhoneGuitarTab.UnitTests/Core/Proxies/FileSystemServiceProxy.cs
+++ trunk/PhoneGuitarTab.UnitTests/Core/Proxies/FileSystemServiceProxy.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
 using PhoneGuitarTab.Core.Services;
 
 namespace PhoneGuitarTab.UnitTests.Core.Proxies
@@ -8,10 +11,45 @@ namespace PhoneGuitarTab.UnitTests.Core.Proxies
 
     public class FileSystemServiceProxy : ProxyBase, IFileSystemService
     {
+        public IEnumerable<string> GetFileNames(string searchPattern = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetDirectoryNames(string searchPattern = null)
+        {
+            throw new NotImplementedException();
+        }
+
         public System.IO.Stream OpenFile(System.String path, System.IO.FileMode mode)
         {
             var methodInvocation = this.BuildMethodInvocation(MethodBase.GetCurrentMethod(), path, mode);
             return this.RunBehaviors(methodInvocation).GetReturnValue<System.IO.Stream>();
+        }
+
+        public Stream OpenFile(string path, FileMode mode, FileAccess access, FileShare share)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FileExists(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteFile(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveFile(string sourceFileName, string destFileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTimeOffset GetLastWriteTime(string path)
+        {
+            throw new NotImplementedException();
         }
 
         public void CreateDirectory(System.String path)
