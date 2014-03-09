405dcd975d5b44d92e0d51a8ca8592a1a3941f97
Halil Karaca <halil.ibrahim.karaca@gmail.com>
2014-03-09 22:44:38 +0100

    Pivot aware AppBar on StartupView through pivots.

--- PhoneGuitarTab.UI/View/StartupView.xaml.cs
+++ PhoneGuitarTab.UI/View/StartupView.xaml.cs
@@ -1,12 +1,27 @@
 ﻿namespace PhoneGuitarTab.UI.View
 {
     using PhoneGuitarTab.Core.Views;
-
+    using PhoneGuitarTab.Controls;
+    using PhoneGuitarTab.UI.ViewModel;
     public partial class StartupView : ViewPage
     {
         public StartupView()
         {
             InitializeComponent();
+        }
+
+        private void Pivot_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+          
+            switch (MainPivot.SelectedIndex)
+            { 
+                case 0:
+                        Bindable.SetApplicationBar(this, (BindableApplicationBar)Resources["TabsAppBar"]); 
+                    break;
+                default:
+                      Bindable.SetApplicationBar(this, (BindableApplicationBar)Resources["Default"]); 
+                    break;
+            }
         }
     }
 }
