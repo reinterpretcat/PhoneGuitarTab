5d9dcbab0fb4751e20990f41f402a22ed692ea66
Halil Karaca <halil.ibrahim.karaca@gmail.com>
2014-03-18 12:01:28 +0100

    - Moved View-Codebehind to ViewModel for Tabs. - Fixed issue with tab navigation with multiselectmode.

--- PhoneGuitarTab.UI/View/Pivots/Tabs.xaml.cs
+++ PhoneGuitarTab.UI/View/Pivots/Tabs.xaml.cs
@@ -12,19 +12,7 @@ namespace PhoneGuitarTab.UI.Pivots
             InitializeComponent();
             
         }
-
-
-        private void TabList_Tap(object sender, System.Windows.Input.GestureEventArgs e)
-        {
-            var selector = sender as LongListMultiSelector;
-            if (!selector.IsSelectionEnabled)
-            {
-                var itemTapped = (System.Windows.FrameworkElement)e.OriginalSource;
-                var tab = (itemTapped.DataContext) as TabEntity;
-                var viewModel = DataContext as CollectionViewModel;
-                viewModel.GoToTabView.Execute(tab);
-            }
-        }
+       
 
         //Unfortunately, not possible to trigger this event via ViewModel
         private void TabList_IsSelectionEnabledChanged(object sender, System.Windows.DependencyPropertyChangedEventArgs e)
