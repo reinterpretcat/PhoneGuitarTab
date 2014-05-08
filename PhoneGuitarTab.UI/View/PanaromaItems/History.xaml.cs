aa72edd29da64f6cd82e5436465ffb3685d1a2bc
Halil Karaca <halil.ibrahim.karaca@gmail.com>
2014-05-08 19:44:42 +0200

    Committing unsynced files Bands/History

---
+++ PhoneGuitarTab.UI/View/PanaromaItems/History.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Phone.Controls;
+using PhoneGuitarTab.UI.Entities;
+using PhoneGuitarTab.UI.ViewModel;
+using PhoneGuitarTab.Controls;
+using PhoneGuitarTab.UI.View;
+
+namespace PhoneGuitarTab.UI.PanaromaItems
+{
+    public partial class History : PanoramaItem
+    {
+        public History()
+        {
+            InitializeComponent();
+        }
+    }
+}
