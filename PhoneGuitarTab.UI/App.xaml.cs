e4cb3845341fe385676615c871d44aec285a96ae
Halil Karaca <halil.ibrahim.karaca@gmail.com>
2014-06-08 23:49:58 +0200

    CForceCulture removed.

--- PhoneGuitarTab.UI/App.xaml.cs
+++ PhoneGuitarTab.UI/App.xaml.cs
@@ -15,7 +15,7 @@ namespace PhoneGuitarTab.UI
 {
     public partial class App : Application
     {
-        public static String AppForceCulture = "pl-pl";
+        public static String AppForceCulture = "";
         /// <summary>
         ///     Constructor for the Application object.
         /// </summary>
